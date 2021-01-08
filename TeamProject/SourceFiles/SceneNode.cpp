4176f1ead136715f70714c22cc62d33e2edd9ef4
alexduarte23 <alex.aires.duarte@gmail.com>
2021-01-08 13:23:55 +0000

    improved draw shader bind

--- TeamProject/SourceFiles/SceneNode.cpp
+++ TeamProject/SourceFiles/SceneNode.cpp
@@ -10,9 +10,14 @@ namespace avt {
 	void SceneNode::draw(Shader* shader, const Mat4& worldMatrix, Light* light) {
 		auto newWorldMat = worldMatrix * getTransform();
 		Shader* curr_shader = _shader ? _shader : shader;
+		if (_shader) {
+			_shader->bind();
+			glUniform3f(curr_shader->getUniform("LightPosition"), light->getPosition().x(), light->getPosition().y(), light->getPosition().z());
+			glUniform3f(curr_shader->getUniform("LightColor"), light->getColor().x(), light->getColor().y(), light->getColor().z());
+		}
 
 		if (_mesh) {
-			curr_shader->bind();
+			//curr_shader->bind();
 			_mesh->va().bind();
 
 			//glEnable(GL_STENCIL_TEST);
@@ -20,8 +25,8 @@ namespace avt {
 			//glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
 			StencilPicker::prepareStencil(_stencilIndex);
 
-			glUniform3f(curr_shader->getUniform("LightPosition"), light->getPosition().x(), light->getPosition().y(), light->getPosition().z());
-			glUniform3f(curr_shader->getUniform("LightColor"), light->getColor().x(), light->getColor().y(), light->getColor().z());
+			//glUniform3f(curr_shader->getUniform("LightPosition"), light->getPosition().x(), light->getPosition().y(), light->getPosition().z());
+			//glUniform3f(curr_shader->getUniform("LightColor"), light->getColor().x(), light->getColor().y(), light->getColor().z());
 
 			beforeDraw();
 			glUniformMatrix4fv(curr_shader->getUniform(MODEL_MATRIX), 1, GL_FALSE, newWorldMat.data());
@@ -31,11 +36,16 @@ namespace avt {
 			//glDisable(GL_STENCIL_TEST);
 
 			_mesh->va().unbind();
-			curr_shader->unbind();
+			//curr_shader->unbind();
 		}
 
 		for (auto childNode : _nodes) {
 			childNode->draw(curr_shader, newWorldMat, light);
+		}
+
+		if (_shader) {
+			_shader->unbind();
+			shader->bind();
 		}
 	}
 
